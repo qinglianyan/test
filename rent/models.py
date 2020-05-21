1d963589794789e68ad9c10a654ca67807063fd8
firefly668 <1205672770@qq.com>
2020-05-21 22:18:41 +0800

    database2

--- rent/models.py
+++ rent/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 class rentOrder(models.Model):
+
+    type=(
+        ("short","短期"),
+        ("long","长期")
+
+    )
+
+    begintime = models.DateField(auto_now_add=True,verbose_name="开始时间")
+    overtime = models.DateField(default=None,verbose_name="结束时间")
     rent_ownUser = models.ForeignKey("login.User",on_delete=models.CASCADE,related_name='rent_ownUser',verbose_name="房东",default=None)
     rent_paidUser = models.ForeignKey("login.User",on_delete=models.CASCADE,related_name='rent_paidUser',verbose_name="房客",default=None)
     house = models.ForeignKey("house.hourse",on_delete=models.CASCADE,default=None)
+    judge = models.BooleanField(verbose_name="审核结果",default=False)
+    type = models.CharField(verbose_name="类型",max_length=32,choices=type)
+
--- repair/models.py
+++ repair/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class repairOrder(models.Model):
+    begintime = models.DateField(auto_now_add=True, verbose_name="开始时间")
+    overtime = models.DateField(default=None, verbose_name="结束时间")
     repair_ownUser = models.ForeignKey("login.User",on_delete=models.CASCADE,related_name='repair_ownUser',verbose_name="房东",default=None)
     repair_paidUser = models.ForeignKey("login.User",on_delete=models.CASCADE,related_name='repair_paidUser',verbose_name="房客",default=None)
     house = models.ForeignKey("house.hourse",on_delete=models.CASCADE,default=None)
+    content = models.CharField(verbose_name="投诉内容",max_length=256)
